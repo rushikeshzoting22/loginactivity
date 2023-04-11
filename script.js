eeb38467d424e1044d6514062b775024a4ce1b97
MilestoneWebstudio <rzoting22@gmail.com>
2023-04-11 13:47:33 +0530

    redirection added

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ submitButton.addEventListener("click", function() {
       console.log("Success! Welcome back!");
       window.alert("Success! Welcome back!");
       // ...
+      window.location.href = "https://flareglobalsoft.com/";
     })
     .catch((error) => {
       const errorCode = error.code;
